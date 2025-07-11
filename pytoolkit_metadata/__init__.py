503209eb2bf6fa7a3612469eef945be67d9d583b
Atsushi Fukuda <example.com>
2025-07-11 11:06:26 +0900

    __init__に公開機能を明記

---
+++ pytoolkit_metadata/__init__.py
@@ -0,0 +1,8 @@
+from .metadata import Project, ProjectUrls, PyProjectToml, get_package_metadata
+
+__all__ = [
+    "get_package_metadata",
+    "PyProjectToml",
+    "Project",
+    "ProjectUrls",
+]
